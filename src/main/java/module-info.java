1acbd6c127ab3d9463dff208e6d467643eeb38a6
GabBilo <201731449@collegealma.ca>
2022-05-03 09:10:17 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.tp4_201731449 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.tp4_201731449 to javafx.fxml;
+    exports com.example.tp4_201731449;
+}
